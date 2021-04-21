a43ec1644be829723ecaab60f82b5686c40f115f
wakeupmh <wakeupmh@gmail.com>
2021-04-21 12:24:57 -0300

    feat(main): handle file

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 const R = require('ramda')
 const fs = require('fs')
 
-const readF = path => fs.readFileSync(path, { enconding: 'utf-8' })
+const readFileSync = path => fs.readFileSync(path, { enconding: 'utf-8' })
+
+const handleReadFile = R.pipe(
+  R.converge(R.concat, [
+    R.pipe(
+      R.replace(/.*\//,''),
+      R.replace(/\..*$/,''),
+      R.concat(R.__, '  ')
+    ),
+    R.pipe(
+      readFileSync,
+      R.toString
+    )
+  ])
+)
 
 R.pipe(
-  R.map(readF),
-  R.map(R.toString),
+  R.map(handleReadFile),
   R.map(R.split('\n')),
   R.map(R.dropLast(1)),
-  R.map(R.map(R.split(' '))),
+  R.map(R.map(R.split('  '))),
+  R.map(R.map(R.take(3))),
   console.log
 )(['../input/eth.csv', '../input/btc.csv'])
